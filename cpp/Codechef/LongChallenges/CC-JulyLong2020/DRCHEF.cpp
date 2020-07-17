d3665169d6a09d2a0f85682403b9e68c14ec1b1c
saxena11ashish <ashishsxn96@gmail.com>
2020-07-17 17:56:12 +0530

    Added solution for CC-july-long20 DRCHEF

--- cpp/Codechef/LongChallenges/CC-JulyLong2020/DRCHEF.cpp
+++ cpp/Codechef/LongChallenges/CC-JulyLong2020/DRCHEF.cpp
@@ -7,76 +7,29 @@ int main() {
         int days=0;
         cin>>N>>drugsAvailable;
         int population[N];
-//        map<int,int> population;
-//        int temp;
-        for(int i=0;i<N;i++){
+        for(int i=0;i<N;i++)
             cin>>population[i];
-            //population[temp]++;
-        }
-//        map<int,int>::iterator itr = population.begin();
         sort(population,population+N);
-        int copy[N];for(int i=0;i<N;i++) copy[i] = population[i];
-        int i=0;
-        while(drugsAvailable>population[i] && i<N){
-            days++;
-            i++;
-        }
+        int i=0,notCured=N;
         while(i<N){
-            if(drugsAvailable == population[i]){
-                days++;
+            if(population[i] >= drugsAvailable){
+                if(population[i] == drugsAvailable){
+                    notCured--;
+                    i++;
+                }
                 drugsAvailable*=2;
+                days++;
+            }
+            else if(2*population[i] >= drugsAvailable){    //drugs should not reduce in number until we reach end of population array
+                notCured--;
+                drugsAvailable = 2*population[i] ;
+                days++;
                 i++;
             }
-            else if(drugsAvailable > population[i]){
-                days++;
-                drugsAvailable = 2* population[i];
+            else
                 i++;
-            }
-            else{
-                days++;
-                population[i] = min(copy[i], 2*(population[i] - drugsAvailable));
-                drugsAvailable*=2;
-            }
         }
-        cout<<days<<endl;
+        cout<<days+notCured<<endl;
     }
     return 0;
 }
-
-
-
-
-
-/*
-#include<bits/stdc++.h>
-using namespace std;
-int main() {
-    int t,N;cin>>t;
-    long long drugsAvailable;
-    while(t--){
-        int days=0;
-        cin>>N>>drugsAvailable;
-        map<int,int> population;
-        int temp;
-        for(int i=0;i<N;i++){
-            cin>>temp;
-            population[temp]++;
-        }
-        map<int,int>::iterator itr = population.begin();
-        while(itr!=population.end()){
-            if(itr->first <= drugsAvailable){
-                if(itr->second==1)
-                    ++itr;
-                else
-                    itr->second-=1;
-            }
-            days++;
-            drugsAvailable*=2;
-        }
-        cout<<days<<endl;
-    }
-    return 0;
-}
-
-
-*/
