a0e5226a47688ecbec528d3ba9dfd9020e6bf92f
saxena11ashish <ashishsxn96@gmail.com>
2020-07-22 21:43:06 +0530

    CF#658Div2: c1

--- cpp/Codeforces/Codeforces Round #658 (Div. 2)/658C1.cpp
+++ cpp/Codeforces/Codeforces Round #658 (Div. 2)/658C1.cpp
@@ -1,28 +1,47 @@
+/*3n operations -- O(n)time
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int t,n;cin>>t;
     string a,b;
+    int t,n;cin>>t;
+    while(t--){
+        cin>>n;
+        cin.ignore();
+        cin>>a>>b;
+        vector<int>v;
+        for(int i=0;i<n;i++){
+            if(a[i]!=b[i]){
+                if(i>0)v.push_back(i+1);
+                v.push_back(1);
+                if(i>0)v.push_back(i+1);
+            }
+        }
+        cout<<v.size();
+        for(int a:v)cout<<" "<<a;
+        cout<<endl;
+    }
+    return 0;
+}
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a,b;
+    int t,n;cin>>t;
     while(t--){
         cin>>n;
         cin.ignore();
         cin>>a>>b;
         vector<int>v;
         for(int i=n-1;i>=0;i--){
-            if(b[i]==a[i])
-                continue;
-            else{
-                if(b[i]==a[0]){
+            if(a[i]!=b[i]){
+                if(a[0]==b[i])
                     v.push_back(1);
-                    v.push_back(i+1);
-                }
-                else{
-                    v.push_back(i+1);
-                }
+                v.push_back(i+1);
             }
         }
         cout<<v.size();
-        for(int i=0;i<v.size();i++)cout<<" "<<v[i];
+        for(int a:v)cout<<" "<<a;
         cout<<endl;
     }
     return 0;
