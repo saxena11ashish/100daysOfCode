f6006d7e4022c558388c5e8ac069f5c9f28b63e5
saxena11ashish <ashishsxn96@gmail.com>
2020-09-12 15:47:20 +0530

    CC SepLong CRDGAME2

---
+++ Codechef/Challenges/SeptemberLong2020/CRDGAME2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define lli             long long int
+#define fastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define deb(x)		 cout<<"\n"<<#x<<" = "<<x<<endl;
+#define deb2(x,y)	 cout<<"\n"<<#x<<" = "<<x<<" | "<<#y<<" = "<<y<<endl;
+#define deba(a)		 cout<<"\n"<<#a<<" : ";for(int i:a)cout<<i<<" ";cout<<endl;
+
+//----------------------------------------------------------------------//
+
+void func(){
+	int n;cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)cin>>a[i];
+// 	sort(a,a+n);
+
+	if(*max_element(a,a+n) == *min_element(a,a+n))
+		n = (n%2==0 ? n/2 : n);
+// 	deb(n);
+	cout<<pow(2,n)<<endl;
+
+}
+
+//----------------------------------------------------------------------//
+int main(){
+fastIO
+#ifndef ONLINE_JUDGE
+	freopen("/home/ashish/CPP/inp.txt","r",stdin);
+   	freopen("/home/ashish/CPP/out.txt","w",stdout);
+#endif
+
+	int t=1;
+	cin>>t;
+	while(t--){
+		func();
+	}
+}
